dd0d8199ba3350e6a712668f46cdf38961f45af7
LéaKugler <leakugler2@gmail.com>
2024-06-08 13:13:25 +0200

    Configuration des pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PageErreurComponent } from './page-erreur/page-erreur.component';
+import { ListeArtisansComponent } from './liste-artisans/liste-artisans.component';
+import { PagesLegalesComponent } from './pages-legales/pages-legales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AccueilComponent},
+  {path: "ListeArtisans", component: ListeArtisansComponent},
+  {path: "mentionslegales", component: PagesLegalesComponent},
+  {path: "accessibilite", component: PagesLegalesComponent},
+  {path: "donneespersonnelles", component: PagesLegalesComponent},
+  {path: "cookies", component: PagesLegalesComponent},
+  {path: "**", component: PageErreurComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
