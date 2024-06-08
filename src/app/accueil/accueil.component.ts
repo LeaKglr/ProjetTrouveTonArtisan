eaabac7ee2e5ffcc8ed1a0bf7d0695f46705d699
LéaKugler <leakugler2@gmail.com>
2024-06-08 13:44:38 +0200

    Ajout page accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ArtisansService, Artisans } from '../artisans.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-accueil',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.scss'
 })
 export class AccueilComponent {
-  title = 'Bienvenue'
+  title = "Comment trouver mon artisan ?"
+  artisansdumois = "Nos 3 meilleurs artisans du mois !"
+  category: string | null = null;
+  filteredArtisans: Artisans[] = [];
+
+  etapes : {title: string, description: string, nombre: number}[] = [
+    {nombre: 1, title: "Choisir la catégorie d'artisanat dans le menu", description: ""},
+    {nombre: 2, title: "Choisir un artisan", description: ""},
+    {nombre: 3, title: "Le contacter via le formulaire de contact", description: ""},
+    {nombre: 4, title: "Une réponse sera apportée sous 48h", description: ""},
+  ]
+
+  constructor(private artisansService : ArtisansService) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.topArtisans = await lastValueFrom(this.artisansService.getTopArtisans());
+
+    } catch (error) {
+      console.error('Erreur', error);
+    }
+  }
+
+  topArtisans: Artisans[] = [];
+
+  getStars(note:number): boolean[] {
+    return Array(5).fill(false).map((_,i) => i < note);
+  }
 }
---
+++ src/app/artisans.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
+export interface Artisans {
+  name: string;
+  note: number;
+  specialty: string;
+  location: string;
+  category: string;
+  about: string;
+  email: string;
+  website: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtisansService {
+
+  private dataUrl = 'assets/datas.json';
+
+  constructor(private http: HttpClient) { }
+
+  getTopArtisans(): Observable<Artisans[]> {
+    return this.http.get<Artisans[]>(this.dataUrl).pipe(
+      map(artisans => {
+        artisans.sort((a,b) => b.note - a.note);
+        return artisans.slice(0, 3);
+      })
+    )
+  }
+}
