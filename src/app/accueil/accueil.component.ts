6ca5966ae243721e1c5d68f3d123b28029dfbd29
LéaKugler <leakugler2@gmail.com>
2024-06-16 17:24:40 +0200

    Explication etape par etape du fonctionnement du site

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -14,10 +14,10 @@ export class AccueilComponent {
   filteredArtisans: Artisans[] = [];
 
   etapes : {title: string, description: string, nombre: number}[] = [
-    {nombre: 1, title: "Choisir la catégorie d'artisanat dans le menu", description: ""},
-    {nombre: 2, title: "Choisir un artisan", description: ""},
-    {nombre: 3, title: "Le contacter via le formulaire de contact", description: ""},
-    {nombre: 4, title: "Une réponse sera apportée sous 48h", description: ""},
+    {nombre: 1, title: "Choisir la catégorie d'artisanat dans le menu", description: "Dans la barre de navigation, vous pouvez trouver un menu avec les différentes catégories des artisans. Ainsi, en cliquant sur la catégorie que vous souhaitez, vous allez pouvoir trouver tous les artisans de ce domaine (Bâtiment, Services, Fabrication et Alimentation)."},
+    {nombre: 2, title: "Choisir un artisan", description: "Une fois que vous avez cliqué sur la catégorie d'artisanat, vous trouverez une liste d'artisan de cette catégorie. Lorsque vous avez choisi votre artisan, cliquez sur sa fiche pour en savoir plus."},
+    {nombre: 3, title: "Le contacter via le formulaire de contact", description: "Si vous êtes satisfait de votre choix, un formulaire de contact vous est proposé. Remplissez les informations nécessaires : nom, objet et message. Puis, cliquez sur le bouton 'Envoyer' afin de contacter l'artisan."},
+    {nombre: 4, title: "Une réponse sera apportée sous 48h", description: "Félicitations ! Le message est envoyé à l'artisan et vous recevrez une réponse sous 48h !"},
   ]
 
   constructor(private artisansService : ArtisansService) {}
